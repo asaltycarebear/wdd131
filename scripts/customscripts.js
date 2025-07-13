d9f9a3a335d6c02ef88042274cbbf3708e0ba749
asaltycarebear <garbear.stockwell@gmail.com>
2025-07-13 16:55:11 -0600

    tab link update

--- scripts/customscripts.js
+++ scripts/customscripts.js
@@ -1,4 +1,4 @@
-function openOrFocusTab(event) {
+function openGoogleFonts(event) {
   event.preventDefault(); // Prevent default link behavior
-  window.open("https://example.com", "exampleTab"); // Reuses the tab if already open
+  window.open("https://fonts.google.com/", "Brose Fonts - Google Fonts"); // Reuses the tab if already open
 }
