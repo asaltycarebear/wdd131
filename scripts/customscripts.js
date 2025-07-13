138cfa6415c0dfe602e47615b346252cb6ab7d56
asaltycarebear <garbear.stockwell@gmail.com>
2025-07-13 16:57:11 -0600

    Name change update

--- scripts/customscripts.js
+++ scripts/customscripts.js
@@ -1,4 +1,4 @@
 function openGoogleFonts(event) {
   event.preventDefault(); // Prevent default link behavior
-  window.open("https://fonts.google.com/", "Brose Fonts - Google Fonts"); // Reuses the tab if already open
+  window.open("https://fonts.google.com/", "Browse Fonts - Google Fonts"); // Reuses the tab if already open
 }
