d658930a7780f6c3b00b7a4ae3dbe2f3aca0a803
asaltycarebear <garbear.stockwell@gmail.com>
2025-07-19 16:32:45 -0600

    week 2 updates

---
+++ week02/scripts/bomscripts.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const li = document.createElement('li');
+const deBut = document.createElement('button');
+
+li.textContent = input.value;
