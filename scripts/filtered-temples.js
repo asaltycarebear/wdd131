821b76de70385e7081752411124f89553d9c69d4
asaltycarebear <garbear.stockwell@gmail.com>
2025-08-02 12:38:30 -0600

    Tried to fix best practices

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -21,7 +21,7 @@ const temples = [
   {
     templeName: "Aba",
     location: "Aba, Nigeria",
-    dedicated: "2005, August, 7",
+    dedicated: new Date("2005-08-07"),
     area: 11500,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
@@ -29,7 +29,7 @@ const temples = [
   {
     templeName: "Manti",
     location: "Manti, Utah, United States",
-    dedicated: "1888, May, 21",
+    dedicated: new Date("1888-05-21"),
     area: 74792,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
@@ -37,7 +37,7 @@ const temples = [
   {
     templeName: "Payson",
     location: "Payson, Utah, United States",
-    dedicated: "2015, June, 7",
+    dedicated: new Date("2015-06-07"),
     area: 96630,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
@@ -45,7 +45,7 @@ const temples = [
   {
     templeName: "Yigo",
     location: "Yigo, Guam",
-    dedicated: "2020, May, 2",
+    dedicated: new Date("2020-05-02"),
     area: 6861,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
@@ -53,7 +53,7 @@ const temples = [
   {
     templeName: "Washington D.C.",
     location: "Kensington, Maryland, United States",
-    dedicated: "1974, November, 19",
+    dedicated: new Date("1974-11-19"),
     area: 156558,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
@@ -61,7 +61,7 @@ const temples = [
   {
     templeName: "Lima",
     location: "Lima, Perú",
-    dedicated: "1986, January, 10",
+    dedicated: new Date("1986-01-10"),
     area: 9600,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
@@ -69,31 +69,31 @@ const temples = [
   {
     templeName: "Mexico City",
     location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
+    dedicated: new Date("1983-12-02"),
     area: 116642,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
     {
     templeName: "City Center",
-    location: "Provo, Utah, USA",
-    dedicated: "2016, March, 20",
+    location: "Provo, Utah, United States",
+    dedicated: new Date("2016-03-20"),
     area: 85084,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/provo-city-center/2018/400x250/Provo-City-Center-Temple03.jpg"
   },
     {
     templeName: "Albuquerque",
-    location: "Albuquerque, New Mexico, USA",
-    dedicated: "2000, March, 5",
+    location: "Albuquerque, New Mexico, United States",
+    dedicated: new Date("2000-03-05"),
     area: 34245,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/400x250/albuquerque-temple-lds-137883-wallpaper.jpg"
   },
     {
     templeName: "Manhattan",
-    location: "Manhattan, New York, USA",
-    dedicated: "2014, June, 13",
+    location: "Manhattan, New York, United States",
+    dedicated: new Date("2014-06-13"),
     area: 20630,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manhattan-new-york/400x250/manhattan-temple-lds-248728-wallpaper.jpg"
@@ -111,19 +111,24 @@ function displayTemples(filteredTemples) {
     card.classList.add("temple-card");
 
     const name = document.createElement("h2"); // Name
-    name.textContent = temple.templeName;
-    name.innerHTML = `${temple.templeName}`;
+    name.textContent = temple.templeName; // created for alt description
+    name.innerHTML = `${temple.templeName}`; // display name
 
     const location = document.createElement("p"); // Location
     location.innerHTML = `<strong>Location:</strong> ${temple.location}`;
 
-    const dedicated = document.createElement("p");
-    dedicated.innerHTML = `<strong>Dedicated:</strong> ${temple.dedicated}`;
+    const dedicated = document.createElement("p"); // Dedication Date
+    const formattedDate = temple.dedicated.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+    dedicated.innerHTML = `<strong>Dedicated:</strong> ${formattedDate.replace(",", "")}`; // Remove extra comma after month
 
-    const area = document.createElement("p");
-    area.innerHTML = `<strong>Area:</strong> ${temple.area.toLocaleString()} sq ft`;
+    const area = document.createElement("p"); // square footages
+    area.innerHTML = `<strong>Area:</strong> ${temple.area.toLocaleString()} sq ft`; //.toLocaleString() formats number
 
-    const image = document.createElement("img");
+    const image = document.createElement("img"); // image location
     image.src = temple.imageUrl;
     image.alt = `Image of ${temple.templeName}`;
     image.loading = "lazy";
